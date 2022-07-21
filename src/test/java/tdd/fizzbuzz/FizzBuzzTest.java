e80d3bb60d81f1c03b37b69f0d7fdeebfefd5e9f
Celia Luo <44634812+Lucelia-L@users.noreply.github.com>
2022-07-21 13:34:51 +0800

    test: should_return_1_when_count_off_given_1

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,21 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+
+    @Test
+    void should_return_1_when_count_off_given_1(){
+        //given
+        int number = 1;
+        String expectedResult = "1";
+        //when
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        String result = fizzBuzz.countOff(number);
+        //then
+        assertEquals(expectedResult,result);
+    }
+
 }
