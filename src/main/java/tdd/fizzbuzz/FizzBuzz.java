5da88e74d888f690d878ab98a166bc8c0a590ec9
Celia Luo <44634812+Lucelia-L@users.noreply.github.com>
2022-07-21 13:39:46 +0800

    feat: implement countOff

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -2,6 +2,10 @@ package tdd.fizzbuzz;
 
 public class FizzBuzz {
     public String countOff(int number) {
+        String result = "";
+        if(number % 3 == 0){
+            return result+="Fizz";
+        }
         return String.valueOf(number);
     }
 }
